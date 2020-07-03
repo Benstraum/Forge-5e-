fb887ea453cf4381c2ba40a25760d1df1f574c6b
ben straumann <benstraum@gmail.com>
2020-07-02 19:17:39 -0500

    nav bar bb

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,38 +1,83 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import anvil from './Anvil.png'
 import './Nav.css';
 
+import { Sidebar, Menu, Icon, Button, Modal } from 'semantic-ui-react';
 
+class Nav extends Component {
+  state = {
+    visible: false,
+    loginModal: false,
+    loggingIn: false
+  }
+  render() {
+    return (
+      <div className="nav">
+        <Sidebar
+          as={Menu}
+          animation='overlay'
+          icon='labeled'
+          inverted
+          vertical
+          visible={this.state.visible}
+          width='thin'
+        >
+          <Menu.Item style={{ textAlign: 'left', minHeight: '40px' }}>
+            <Icon inverted size="large" onClick={() => this.setState({ ...this.state, visible: !this.state.visible })} name='close' />
+          </Menu.Item>
+          <Menu.Item style={{ textAlign: 'center', minHeight: '40px' }}>
+          {
+              this.props.user.id ?
+              <>
+                <Link to='/home'>
+                <Icon  name='home' /><br/>
+                Home
+                </Link>
+              </>
+              :
+              <>
+                <Link to='/home' onClick={() => this.setState({ ...this.state, visible: !this.state.visible })}>
+                Login/Register
+                </Link>
+              </>
+            }
+          </Menu.Item>
+          <Menu.Item style={{ textAlign: 'center', minHeight: '40px' }}>
+          {
+              <>
+                <Link to='/about' onClick={()=>this.setState({...this.state,visible:!this.state.visible})}>
+                <Icon size="large"  name='info circle' /><br/>
+                about
+                </Link>
+              </>
+             
+            }
+          </Menu.Item>
+          <Menu.Item style={{ textAlign: 'center', minHeight: '40px' }} onClick={()=>this.setState({...this.state,visible:!this.state.visible})}>
+          {this.props.user.id && (
+            < >
+              <LogOutButton className="nav-link" />
+            </>
+          )}
+          </Menu.Item>
+        </Sidebar>
 
-const Nav = (props) => {
-return (
-  <div className="nav">
-    <img alt='anvil'src={anvil} />
-    <Link to="/home">
-      <h2 className="nav-title">Forge</h2>
-    </Link>
-    <div className="nav-right">
-      <Link className="nav-link" to="/home">
-        {/* Show this link if they are logged in or not,
-        but call this link 'Home' if they are logged in,
-        and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
-      </Link>
-      <Link className="nav-link" to="/about">
-        About
-      </Link>
-      {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
-        <>
-          <LogOutButton className="nav-link"/>
-        </>
-      )}
-    </div>
-  </div>
-)};
+            <Icon className="icon" size='big' onClick={()=>this.setState({...this.state,visible:!this.state.visible})} name='bars' />
+  
+        
+        <div className="nav-right">
+        <Link to="/home">
+          <h2 className="nav-title">Forge</h2>
+          <img alt='anvil' src={anvil} />
+        </Link>
+        </div>
+      </div>
+    )
+  }
+};
 
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
