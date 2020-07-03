bbd8d6d71db4d5c3bd59e22476212d0c8e9a6b82
ben straumann <benstraum@gmail.com>
2020-07-03 17:58:13 -0500

    slimmed down progress

--- src/components/3Class/Class.js
+++ src/components/3Class/Class.js
@@ -43,37 +43,31 @@ class Class extends Component {
         <Step >
           <Step.Content>
             <Step.Title>Race</Step.Title>
-            <Step.Description>Choose your player race</Step.Description>
           </Step.Content>
         </Step>
         <Step active>
           <Step.Content>
             <Step.Title>Class</Step.Title>
-            <Step.Description>Choose your class</Step.Description>
           </Step.Content>
         </Step>
         <Step disabled>
           <Step.Content>
             <Step.Title>Items</Step.Title>
-            <Step.Description>choose starting gear</Step.Description>
           </Step.Content>
         </Step>
         <Step disabled>
           <Step.Content>
             <Step.Title>Stats</Step.Title>
-            <Step.Description>Allocate Stats</Step.Description>
           </Step.Content>
         </Step>
         <Step disabled>
           <Step.Content>
             <Step.Title>Skills</Step.Title>
-            <Step.Description>Choose Skills</Step.Description>
           </Step.Content>
         </Step>
         <Step disabled>
           <Step.Content>
             <Step.Title>Name</Step.Title>
-            <Step.Description>Name Your Character</Step.Description>
           </Step.Content>
         </Step>
       </Step.Group>
--- src/components/8ReviewAndName/ReviewAndName.js
+++ src/components/8ReviewAndName/ReviewAndName.js
@@ -25,42 +25,36 @@ class ReviewAndName extends Component {
       <Step.Group size='tiny' widths={8} unstackable>
         <Step >
           <Step.Content>
-            <Step.Title>Race</Step.Title>
-            <Step.Description>Choose your player race</Step.Description>
+            <Step.Title>Race</Step.Title> 
           </Step.Content>
         </Step>
         <Step >
           <Step.Content>
             <Step.Title>Class</Step.Title>
-            <Step.Description>Choose your class</Step.Description>
           </Step.Content>
         </Step>
         <Step >
           <Step.Content>
             <Step.Title>Items</Step.Title>
-            <Step.Description>choose starting gear</Step.Description>
           </Step.Content>
         </Step>
         <Step >
           <Step.Content>
             <Step.Title>Stats</Step.Title>
-            <Step.Description>Allocate Stats</Step.Description>
           </Step.Content>
         </Step>
         <Step >
           <Step.Content>
             <Step.Title>Skills</Step.Title>
-            <Step.Description>Choose Skills</Step.Description>
           </Step.Content>
         </Step>
         <Step active>
           <Step.Content>
             <Step.Title>Name</Step.Title>
-            <Step.Description>Name Your Character</Step.Description>
           </Step.Content>
         </Step>
       </Step.Group>
-      <h2>Who are you?</h2>
+      <h2 style={{textAlign:'center'}}>Who are you?</h2>
       <Input fluid placeholder='What is your name?' onChange={(event) => this.handleChange(event, 'name')} />
       <h4 style={{textAlign:'center'}}> the {this.props.char.race.name} {this.props.char.class.class_name}</h4>
       <br />
